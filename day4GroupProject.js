995a32c5d2b576dd0e779f4974560f761aab7e53
sado8sh <saadhamdi507@gmail.com>
2025-08-04 12:40:53 +0100

    day 4 group project

---
+++ day4GroupProject.js
@@ -0,0 +1,66 @@
+let numbers = [1, 23, 13, 12, 5, 8, 6, 2, 4, 10];
+function bubbleSort(arr) {
+    let n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+            }
+        }
+    }
+    return arr;
+}
+console.log(`Sorted array: [${bubbleSort(numbers)}]`);
+function selectionSort(arr) {
+    let n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
+    return arr;
+}
+console.log(`Sorted array: [${selectionSort(numbers)}]`);
+function insertionSort(arr) {
+    let n = arr.length;
+    for (let i = 1; i < n; i++) {
+        let key = arr[i];
+        let j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+    return arr;
+}
+console.log(`Sorted array: [${insertionSort(numbers)}]`);
+
+function linearSearch(arr, target) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === target) {
+            return i;
+        }
+    }
+}
+number = 4;
+console.log(`Index of ${number} in array: ${linearSearch(numbers, number)}`);
+function binarySearch(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (arr[mid] === target) {
+            return mid;
+        } else if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+}
+console.log(`Index of ${number} in sorted array: ${binarySearch(numbers, number)}`);
